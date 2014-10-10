fab37ccd7db3d209688f313ea577388b1ce3367c
Adam Bergen <bergen@gmail.com>
2014-10-10 00:04:08 -0500

    setup array and foreach

--- hw13.js
+++ hw13.js
@@ -1,1 +1,40 @@
-console.log("test");
+//console.log("test");
+
+var phones = [{
+    brand: "Apple",
+    model: "iPhone6",
+    color: "gold"
+}, {
+    brand: "Apple",
+    model: "iPhone6Plus",
+    color: "gold"
+}, {
+    brand: "Apple",
+    model: "iPhone5S",
+    color: "white"
+}, {
+    brand: "Samsung",
+    model: "GalaxyS5",
+    color: "white"
+}, {
+    brand: "Samsung",
+    model: "GalaxyNote",
+    color: "black"
+}, {
+    brand: "HTC",
+    model: "One",
+    color: "silver"
+}, {
+    brand: "Nokia",
+    model: "Lumia",
+    color: "black"
+}];
+
+console.log(phones);
+
+function ForEach(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+        callback(list[i], i);
+    }
+}
+
